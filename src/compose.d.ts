0e3ee7b842ea61d59e3eedff2d135498bf6df180
Eric Andre <ericandre615@gmail.com>
2021-02-09 16:29:12 -0600

    change name, already taken

--- src/compose.d.ts
+++ src/compose.d.ts
@@ -1,4 +1,4 @@
-declare module 'react-compose-hooks' {
+declare module 'compose-hooks-react' {
   export function composeHooks(...hooks: Function[]): <P extends object>(Component: React.ComponentType<P>) => (props: Object) => JSX.Element;
   export default composeHooks;
 }
